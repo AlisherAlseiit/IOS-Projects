21f5d77b1aedf59b077290a7faa6e36e3fc6ea48
Alisher <Alisher>
2021-09-01 23:28:11 +0600

    charts app

---
+++ Charts/ChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsApp.swift
+//  Charts
+//
+//  Created by Алишер Алсейт on 17.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct ChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
